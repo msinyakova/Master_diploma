ae265ad45d4ca018bdf15d54026e5b58f6eb3345
Sinyakova Marina <marinasin19@gmail.com>
2021-05-06 16:19:05 +0300

    Rewrite rho_a and b_a calculation. Add building queue organization

--- algorithm.py
+++ algorithm.py
@@ -1,21 +1,68 @@
 import sys
 import json
 import csv
+import math
 
 import objects
 import slicedelay
 
+DELTA_DELAY = 0.8
+
 
 # сортируем слайсы в зависимости от требования к задержке
 def sort_slices(slices, order):
-    print('sort slices by delay')
-    # TODO
+    for key in slices.keys():
+        pos = 0
+        for i in range(0, len(order)):
+            if slices[order[i]].qos_delay < slices[key].qos_delay:
+                pos += 1
+            else:
+                break
+        order.insert(pos, key)
 
 
 # задаем начальные значения приоритетов и весов для виртуальных пластов на каждом коммутаторе
 def set_initial_parameters(slices, slices_order, topology):
-    print('set_initial_parameters')
-    # TODO
+    # для каждого слайса создаем множество коммутаторов, через которое проходят потоки
+    for sls in slices.keys():
+        for flow in slices[sls].flows_list:
+            for sw in flow.path:
+                slices[sls].sls_sw_set.add(sw)
+        # print(slices[sls].sls_sw_set)
+
+    # для каждоко слайса в порядке slices_order на каждом коммутаторе из sls_sw_set
+    # создаем очереди о объединяем их в приоритеты
+    for sls in slices_order:
+        for sw in slices[sls].sls_sw_set:
+            # создаем очередь для слайса
+            queue = objects.Queue(sls, slices[sls], 1, sw)
+            if len(topology.switches[sw].priority_list) == 0:
+                # создаем приоритет для слайса
+                priority = objects.Priority(1, slices[sls].qos_throughput, slices[sls].qos_delay, queue)
+                topology.switches[sw].priority_list.append(priority)
+            else:
+                was_added = False
+                for pr in topology.switches[sw].priority_list:
+                    if math.fabs(pr.mean_delay - slices[sls].qos_delay) < DELTA_DELAY:
+                        was_added = True
+                        # добавляем очередь в существующий приоритет
+                        queue.priority = pr.priority
+                        pr.queue_list.append(queue)
+                        pr.throughput += slices[sls].qos_throughput
+                        pr.recalculation()
+                if not was_added:
+                    # создаем новый приоритет и туда добавляем очередь
+                    number = len(topology.switches[sw].priority_list) + 1
+                    priority = objects.Priority(number, slices[sls].qos_throughput, slices[sls].qos_delay, queue)
+                    topology.switches[sw].priority_list.append(priority)
+
+    # print('Start queue organization:')
+    # for sw in topology.switches.keys():
+    #     print('----------- switch', sw, '------------------')
+    #     for pr in topology.switches[sw].priority_list:
+    #         print(pr.priority, ':', 'throughput =', pr.throughput, 'mean_delay =', pr.mean_delay)
+    #         for queue in pr.queue_list:
+    #             print('queue', queue.number, ': weight =', queue.weight, 'flows_number =', queue.flow_numbers)
 
 
 # формируем кривую обслуживания на каждом коммутаторе для начальных параметров
@@ -50,6 +97,7 @@ def parse_config(input_file, slices, topology):
 
         # считываем слайсы
         for sls_data in data["slices"]:
+            correct = True
             sls = objects.Slice(sls_data["sls_number"], sls_data["qos_throughput"], sls_data["qos_delay"])
             for fl in sls_data["flows"]:
                 flow = objects.Flow(fl["epsilon"], fl["path"])
@@ -57,12 +105,18 @@ def parse_config(input_file, slices, topology):
                     with open(fl["statistic"], 'r') as f:
                         reader = csv.reader(f)
                         stat_list = list(reader)
-                    flow.define_distribution(stat_list, chi_square)
+                    rate = topology.switches[flow.path[0]].physical_speed
+                    flow.define_distribution(stat_list, chi_square, rate)
+                    if flow.rho_a == 0 and flow.b_a == 0:
+                        print("Reject slice installation")
+                        correct = False
+                        break
                 else:
                     flow.rho_a = fl["rho_a"]
                     flow.b_a = fl["b_a"]
                 sls.flows_list.append(flow)
-            slices[sls.id] = sls
+            if correct:
+                slices[sls.id] = sls
 
 
 # записываем результаты работы в выходной файл
@@ -79,7 +133,7 @@ def main(argv):
     parse_config(argv[0], slices, topology)
 
     # сортируем слайсы в зависимости от требования к задержке
-    slices_order = list()
+    slices_order = list()       # список номеров слайсов, упорядоченный по возрастанию задержки
     sort_slices(slices, slices_order)
 
     # задаем начальные значения приоритетов и весов для виртуальных пластов на каждом коммутаторе
--- objects.py
+++ objects.py
@@ -1,20 +1,38 @@
+import math
+
+
 class Queue:
-    def __init__(self, number_):
+    def __init__(self, number_, slice_, priority_, sw):
         self.number = number_       # номер очереди == номеру слайса
-        self.priority = 0           # приоритет очереди на коммутаторе
-        self.weight = 0             # доля пропускной способности приоритета (omega)
-        self.slice = 0              # указать на слайс, который передает в этой очереди
+        self.priority = priority_   # приоритет очереди на коммутаторе
+        self.weight = 1.0           # доля пропускной способности приоритета (omega)
+        self.slice = slice_         # указать на слайс, который передает в этой очереди
         self.rho_s = 0              # скорость для кривой обслуживания
         self.b_s = 0                # задержка для кривой обслуживания
-        self.route_numbers = 0      # количество маршрутов слайса, проходящих через этот коммутатор
+        self.flow_numbers = 0       # количество маршрутов слайса, проходящих через этот коммутатор
+        self.add_flow_number(sw)
+
+    def add_flow_number(self, sw):
+        for flow in self.slice.flows_list:
+            for elem in flow.path:
+                if elem == sw:
+                    self.flow_numbers += 1
 
 
 class Priority:
-    def __init__(self, number_):
-        self.priority = number_     # значение приоритета
-        self.throughput = 0         # пропускная способность приоритета
-        self.delay = 0              # суммарная задержка приоритета
-        self.queue_list = list()    # список очередей, входящих в приоритет
+    def __init__(self, number_, throughput_, qos_delay_, queue):
+        self.priority = number_         # значение приоритета
+        self.throughput = throughput_   # пропускная способность приоритета
+        self.queue_list = [queue]       # список очередей, входящих в приоритет
+        self.mean_delay = qos_delay_    # среднее требование по задержка приоритета
+        self.delay = 0                  # суммарная задержка приоритета
+
+    def recalculation(self):
+        self.mean_delay = 0.0
+        for queue in self.queue_list:
+            self.mean_delay += queue.slice.qos_delay
+            queue.weight = queue.slice.qos.throughput / self.throughput
+        self.mean_delay /= len(self.queue_list)
 
 
 class Switch:
@@ -37,9 +55,43 @@ class Flow:
         self.epsilon = eps_     # вероятность ошибки оценки кривой нагрузки
         self.path = path_       # список коммутатор, через которые проходит поток
 
-    def define_distribution(self, stat_list, chi_square):
-        print(self.rho_a, self.b_a)
-        # TODO
+# формирование кривой нагрузки. На вход подается файл со статистикой,
+# распределение хи квадрат и физическая скорость канала первого коммутатора в пути потока
+    def define_distribution(self, stats, chi_square, rate):
+        # вычисляем сколько раз встречается каждое значение x_s
+        x_s = dict()
+        s = 0
+        for st in stats:
+            elem = int(float(st[0]))
+            if x_s.get(elem, -1) != -1:
+                x_s[elem] += 1
+            else:
+                x_s[elem] = 1
+                s += 1
+
+        # находим общее число элементов - sum_n и математическое ожидание sum_xn = sum(x_s * n_s)
+        sum_n = sum_xn = 0
+        for key in x_s.keys():
+            sum_xn += key * x_s[key]
+            sum_n += x_s[key]
+        lambda_medium = sum_xn / sum_n
+        # print('Poisson distribution with lambda = ', lambda_medium)
+        # вычисляем вероятность p_s прихода элемента x_s, слагаемое Пирсона K_s и суммарное K
+        k_sum = 0
+        for key in x_s.keys():
+            p_s = lambda_medium ** key / math.factorial(key) * math.exp(-lambda_medium)
+            k_s = (x_s[key] - sum_n * p_s) ** 2 / sum_n * p_s
+            k_sum += k_s
+        # print('K_критическое = ', chi_square[s - 1], ' K_наблюдаемое = ', k_sum)
+        if chi_square[s - 1] >= k_sum:
+            self.rho_a = lambda_medium
+            sigma_a = 0
+            theta = 100000
+            self.b_a = sigma_a - 1 / theta * (
+                    math.log(self.epsilon) + math.log(1 - math.exp(-theta * (rate - self.rho_a))))
+            # print('rho_a = ', self.rho_a, ' b_a = ', self.b_a)
+        else:
+            print('Not poisson distribution')
 
 
 class Slice:
@@ -48,4 +100,5 @@ class Slice:
         self.qos_throughput = throughput_   # требования к пропускной способности слайса
         self.qos_delay = delay_             # требования к задержке слайса
         self.flows_list = list()            # список маршрутов
+        self.sls_sw_set = set()             # множество коммутаторв, через которые проходят потоки слайса
 
